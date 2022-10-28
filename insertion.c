7a0021de756bcd66b9efe3e1347116145a070ba1
Laura Castillo <102078623+LauCastle@users.noreply.github.com>
2022-10-28 10:26:21 -0500

    se mejoró insertion y eliminé .exe

--- insertion.c
+++ insertion.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //Estructura de Datos 3°D
 //Insertion Sort
+int a[5];
+
 
 void intercambio(int *x, int *y)// en ingles swap().
 {
@@ -57,6 +60,29 @@ void bubbleSort(int a[], int n){
 				intercambio(&a[j],&a[j+1]);
 }
 
+void leer_archivo(){
+	FILE* archivo;
+	char texto[20];
+	char c;
+	int j=0;
+	int i=0;
+
+	archivo = fopen("datos.txt", "rt");
+	do{
+		c = fgetc(archivo);
+		if(c == ','){
+			a[j] = atoi(texto);
+			j++;
+			for(int z=0; z<i; z++)
+				texto[z]=0;
+			i=0;
+			continue;
+		}
+		texto[i] = c;
+		i++;
+	}while(c!=EOF);
+}
+
 void pantalla(int a[], int n){
 	for(int j=0; j<n-1; j++)
 		printf("%d, ", a[j]);
@@ -64,21 +90,14 @@ void pantalla(int a[], int n){
 }
 
 void main(){
-	int t=0;
-	printf("Ingrese los numeros a permutar: ");
-	scanf("%d",&t);
-
-	int a[t];
-	for(int i=0; i<t; i++){
-		printf("Ingrese el valor de %d: ",i+1);
-		scanf("%d",&a[i]);
-	}
+	int t=5;
+	leer_archivo(a,t);
 
 	printf("\nValores desordenados: ");
 	pantalla(a,t);
 	//bubbleSort(a,t);
 	//insertion_sort(a,t);
-	quicksort(a,0,t);
+	quicksort(a,0,t-1);
 	printf("\nValores ordenados: ");
 	pantalla(a,t);
 	printf("\n\n");
